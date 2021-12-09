8a01ea2e1c61b0b47d68ac80a0399df0297d3bc0
josep-p <jose.pinzonp@gmail.com>
2021-12-09 16:28:00 -0500

    Añadir objetivos a la tabla

--- src/pages/proyecto/indexLider.js
+++ src/pages/proyecto/indexLider.js
@@ -1,4 +1,4 @@
-import React/* , { useEffect } */ from 'react';
+import React, { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_PROYECTOS_LIDERADOS } from 'graphql/proyecto/queries';
 import { Link } from 'react-router-dom';
@@ -8,6 +8,13 @@ import DropDown from 'components/DropDown'
 import { Enum_EstadoProyecto, Enum_FaseProyecto } from 'utils/enums';
 import useFormData from 'hooks/useFormData';
 import Input from 'components/Input';
+import { CREAR_PROYECTO } from 'graphql/proyecto/mutations';
+import { useUser } from 'context/userContext';
+import { Enum_TipoObjetivo } from 'utils/enums';
+import { nanoid } from 'nanoid';
+import { ObjContext } from 'context/objContext';
+import { useObj } from 'context/objContext';
+
 
 const IndexProyectosLider = () => {
   const { data, error, loading } = useQuery(GET_PROYECTOS_LIDERADOS, {variables:{
@@ -147,6 +154,7 @@ const IndexProyectosLider = () => {
                     <th>Estado</th>
                     <th>Fase</th>
                     <th>Lider</th>
+                    <th>Objetivos</th>
                     <th>Detalles</th>
                     <th>Editar</th>
                     
@@ -196,3 +204,73 @@ const IndexProyectosLider = () => {
 };
 
 export default IndexProyectosLider
+
+const Objetivos = () => {
+  const [listaObjetivos, setListaObjetivos] = useState([]);
+
+  const eliminarObjetivo = (id) => {
+    setListaObjetivos(listaObjetivos.filter((el) => el.props.id !== id));
+  };
+
+  const componenteObjetivoAgregado = () => {
+    const id = nanoid();
+    return <FormObjetivo key={id} id={id} />;
+  };
+
+  useEffect(() => {
+
+  }, [listaObjetivos]);
+
+  return (
+    <ObjContext.Provider value={{ eliminarObjetivo }}>
+      <div>
+        <span>Objetivos del Proyecto</span>
+        {
+          <i
+            onClick={() => setListaObjetivos([...listaObjetivos, componenteObjetivoAgregado()])}
+            className='fas fa-plus rounded-full bg-indigo-700 hover:bg-indigo-500 text-white p-2 mx-2 cursor-pointer'
+          />
+        }
+        {listaObjetivos.map((objetivo) => {
+          return objetivo;
+        })}
+      </div>
+    </ObjContext.Provider>
+  );
+};
+
+const Objetivo = ({ tipo, descripcion }) => {
+  return (
+    <div>
+      <div className='text-lg font-bold'>{tipo}</div>
+      <div>{descripcion}</div>
+     {/*  <PrivateComponent roleList={['ADMINISTRADOR']}>
+        <div>Editar</div>
+      </PrivateComponent> */}
+    </div>
+  );
+};
+
+const FormObjetivo = ({ id }) => {
+  const { eliminarObjetivo } = useObj();
+  return (
+    <div className='flex items-center'>
+      <Input
+        name={`nested||objetivos||${id}||descripcion`}
+        label='Descripción'
+        type='text'
+        required={true}
+      />
+      <DropDown
+        name={`nested||objetivos||${id}||tipo`}
+        options={Enum_TipoObjetivo}
+        label='Tipo de Objetivo'
+        required={true}
+      />
+      <i
+        onClick={() => eliminarObjetivo(id)}
+        className='fas fa-trash rounded-full bg-indigo-700 hover:bg-indigo-500 text-white p-2 mx-2 cursor-pointer mt-6'
+      />
+    </div>
+  );
+};
