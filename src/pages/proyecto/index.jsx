42dda2c31d83351dc78fb532f6f075d4a7fdf53b
paulandreacl <paula150995@hotmail.com>
2021-12-10 18:37:21 -0500

    acordion activar proyecto

--- src/pages/proyecto/index.jsx
+++ src/pages/proyecto/index.jsx
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { useQuery, useMutation } from '@apollo/client';
 
 import { GET_USUARIOS } from 'graphql/usuario/queries';
-import {  PROYECTO_FASE_EDITADO } from 'graphql/proyecto/mutations';
+import { PROYECTO_FASE_EDITADO } from 'graphql/proyecto/mutations';
 
 import { GET_PROYECTOS, GET_PROYECTO } from 'graphql/proyecto/queries';
-import { CREAR_INSCRIPCION  } from 'graphql/proyecto/mutations';
+import { CREAR_INSCRIPCION } from 'graphql/proyecto/mutations';
 import { Link, useNavigate } from 'react-router-dom';
 import './proyecto.css'
 import ButtonLoading from 'components/ButtonLoading';
@@ -19,6 +19,7 @@ import { nanoid } from 'nanoid';
 import { ObjContext } from 'context/objContext';
 import { useObj } from 'context/objContext';
 import IndexProyectosLider from './indexLider';
+import ActivarProyecto from './activarProyecto';
 
 
 const IndexProyectos = () => {
@@ -29,17 +30,9 @@ const IndexProyectos = () => {
   const [crearProyecto, { data: mutationData, loading: mutationLoading, error: mutationError }] =
     useMutation(CREAR_PROYECTO);
 
-  const [activar,{data: dataMutation1, loading: loadingMutation1,
-    error: errorMutaton1}]=useMutation(PROYECTO_FASE_EDITADO);
+  const [activar, { data: dataMutation1, loading: loadingMutation1,
+    error: errorMutaton1 }] = useMutation(PROYECTO_FASE_EDITADO);
 
-  const activarProyecto = (e) => {
-      activar({variables:{
-        id: e._id,
-        fechaInicio: Date.now(),
-        estado: "ACTIVO",
-        fase: "INICIADO",
-      }});    
-  };
 
   useEffect(() => {
 
@@ -63,27 +56,97 @@ const IndexProyectos = () => {
   // const { data: dataMutation2, loading: loadingMutation2,
   //   error: errorMutaton2 } = useQuery(GET_PROYECTO);
 
-  const [inscribir,{data: dataMutation2, loading: loadingMutation2,
-    error: errorMutaton2}]=useMutation(CREAR_INSCRIPCION);
-  
-  const navigate=useNavigate('/inscripciones',{replace:true});
+  const [inscribir, { data: dataMutation2, loading: loadingMutation2,
+    error: errorMutaton2 }] = useMutation(CREAR_INSCRIPCION);
+
+  const navigate = useNavigate('/inscripciones', { replace: true });
 
   const inscripcion = (proyecto) => {
     console.log(proyecto._id)
-     inscribir({variables:{
-       proyecto: proyecto._id,
-       estudiante:'61a83b3e5300014bdd95e3eb'   /* Warning colocar info del usuario que se registro inicialmente*/
-     }}
-     );
-     navigate('/inscripciones',{replace:true});
-     window.location.reload(true)
+    inscribir({
+      variables: {
+        proyecto: proyecto._id,
+        estudiante: '61a83b3e5300014bdd95e3eb'   /* Warning colocar info del usuario que se registro inicialmente*/
+      }
+    }
+    );
+    navigate('/inscripciones', { replace: true });
+    window.location.reload(true)
   };
 
+  const Objetivos = () => {
+    const [listaObjetivos, setListaObjetivos] = useState([]);
+
+    const eliminarObjetivo = (id) => {
+      setListaObjetivos(listaObjetivos.filter((el) => el.props.id !== id));
+    };
+
+    const componenteObjetivoAgregado = () => {
+      const id = nanoid();
+      return <FormObjetivo key={id} id={id} />;
+    };
+
+    useEffect(() => {
+
+    }, [listaObjetivos]);
+
+    return (
+      <ObjContext.Provider value={{ eliminarObjetivo }}>
+        <div>
+          <span>Objetivos del Proyecto</span>
+          {
+            <i
+              onClick={() => setListaObjetivos([...listaObjetivos, componenteObjetivoAgregado()])}
+              className='fas fa-plus rounded-full bg-indigo-700 hover:bg-indigo-500 text-white p-2 mx-2 cursor-pointer'
+            />
+          }
+          {listaObjetivos.map((objetivo) => {
+            return objetivo;
+          })}
+        </div>
+      </ObjContext.Provider>
+    );
+  };
+
+  const Objetivo = ({ tipo, descripcion }) => {
+    return (
+      <div>
+        <div className='text-lg font-bold'>{tipo}</div>
+        <div>{descripcion}</div>
+        {/*  <PrivateComponent roleList={['ADMINISTRADOR']}>
+          <div>Editar</div>
+        </PrivateComponent> */}
+      </div>
+    );
+  };
+
+  const FormObjetivo = ({ id }) => {
+    const { eliminarObjetivo } = useObj();
+    return (
+      <div className='flex items-center'>
+        <Input
+          name={`nested||objetivos||${id}||descripcion`}
+          label='Descripción'
+          type='text'
+          required={true}
+        />
+        <DropDown
+          name={`nested||objetivos||${id}||tipo`}
+          options={Enum_TipoObjetivo}
+          label='Tipo de Objetivo'
+          required={true}
+        />
+        <i
+          onClick={() => eliminarObjetivo(id)}
+          className='fas fa-trash rounded-full bg-indigo-700 hover:bg-indigo-500 text-white p-2 mx-2 cursor-pointer mt-6'
+        />
+      </div>
+    );
+  };
 
   if (loading) return <div>Loading...</div>;
 
   if (error) return <div>Error...</div>;
-
 
   return (
     <div>
@@ -159,9 +222,8 @@ const IndexProyectos = () => {
                     <th>Estado</th>
                     <th>Fase</th>
                     <th>Lider</th>
-                    <th>Objetio</th>
+                    <th>Objetivo</th>
                     <th>Editar</th>
-                    <th>Acti proyec</th>
                     <th>Incripción</th>
                   </tr>
                 </thead>
@@ -178,18 +240,14 @@ const IndexProyectos = () => {
                           <td>{u.fase}</td>
                           <td>{u.lider.nombre + ' ' + u.lider.apellido}</td>
                           <td> {u.objetivos.map((objetivo) => {
-                          return <Objetivo tipo={objetivo.tipo} descripcion={objetivo.descripcion} />;
-                        })}</td>
+                            return <Objetivo tipo={objetivo.tipo} descripcion={objetivo.descripcion} />;
+                          })}</td>
                           <td className='py-3 px-5'>
                             <Link to={`/proyectos/editar/${u._id}`}>
                               <i className='fas fa-pen text-yellow-600 hover:text-yellow-400 cursor-pointer' />
                             </Link>
                           </td>
-                          <td>{u.estado == "INACTIVO" && u.fase == "NULO" ? <td className='py-2 px-5'>
-                            <button><i onClick={()=>{activarProyecto(u)}} className='fas fa-check-circle text-green-600 hover:text-yellow-400
-                            cursor-pointer'/></button>
-                          </td> : ""}</td>
-                          <td className='py-3 px-5'> <button><i onClick={()=>{inscripcion(u)}} className='fas fa-plus-circle text-green-600 hover:text-yellow-400
+                          <td className='py-3 px-5'> <button><i onClick={() => { inscripcion(u) }} className='fas fa-plus-circle text-green-600 hover:text-yellow-400
                             cursor-pointer'/></button></td>
                         </tr>
                       );
@@ -201,82 +259,15 @@ const IndexProyectos = () => {
           </div>
         </div >
       </div >
-     <div>
-       <IndexProyectosLider></IndexProyectosLider></div> 
-    </div>
-  );
-};
 
-const Objetivos = () => {
-  const [listaObjetivos, setListaObjetivos] = useState([]);
-
-  const eliminarObjetivo = (id) => {
-    setListaObjetivos(listaObjetivos.filter((el) => el.props.id !== id));
-  };
-
-  const componenteObjetivoAgregado = () => {
-    const id = nanoid();
-    return <FormObjetivo key={id} id={id} />;
-  };
-
-  useEffect(() => {
-
-  }, [listaObjetivos]);
-
-  return (
-    <ObjContext.Provider value={{ eliminarObjetivo }}>
       <div>
-        <span>Objetivos del Proyecto</span>
-        {
-          <i
-            onClick={() => setListaObjetivos([...listaObjetivos, componenteObjetivoAgregado()])}
-            className='fas fa-plus rounded-full bg-indigo-700 hover:bg-indigo-500 text-white p-2 mx-2 cursor-pointer'
-          />
-        }
-        {listaObjetivos.map((objetivo) => {
-          return objetivo;
-        })}
-      </div>
-    </ObjContext.Provider>
-  );
-};
 
-const Objetivo = ({ tipo, descripcion }) => {
-  return (
-    <div>
-      <div className='text-lg font-bold'>{tipo}</div>
-      <div>{descripcion}</div>
-     {/*  <PrivateComponent roleList={['ADMINISTRADOR']}>
-        <div>Editar</div>
-      </PrivateComponent> */}
+        <IndexProyectosLider></IndexProyectosLider></div>
+
+      {/* <div><ActivarProyecto /></div> */}
+
     </div>
   );
 };
-
-const FormObjetivo = ({ id }) => {
-  const { eliminarObjetivo } = useObj();
-  return (
-    <div className='flex items-center'>
-      <Input
-        name={`nested||objetivos||${id}||descripcion`}
-        label='Descripción'
-        type='text'
-        required={true}
-      />
-      <DropDown
-        name={`nested||objetivos||${id}||tipo`}
-        options={Enum_TipoObjetivo}
-        label='Tipo de Objetivo'
-        required={true}
-      />
-      <i
-        onClick={() => eliminarObjetivo(id)}
-        className='fas fa-trash rounded-full bg-indigo-700 hover:bg-indigo-500 text-white p-2 mx-2 cursor-pointer mt-6'
-      />
-    </div>
-  );
-};
-
-
 
 export default IndexProyectos
