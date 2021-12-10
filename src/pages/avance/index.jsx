7a71dd444710add1cf1cd7b2c742b23680554afe
Arturo-Nino <dieaninotor@unal.edu.co>
2021-12-09 20:36:36 -0500

    Nuevo acordeón

--- src/pages/avance/index.jsx
+++ src/pages/avance/index.jsx
@@ -12,6 +12,8 @@ import Input from 'components/Input';
 const IndexAvances = () => {
   const { data, error, loading } = useQuery(GET_AVANCES);
 
+  
+
   if (loading) return <div>Loading...</div>;
 
   if (error) return <div>Error...</div>;
@@ -116,6 +118,62 @@ const IndexAvances = () => {
             </button>
           </h2>
           <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo"
+            data-bs-parent="#accordionExample">
+            <div className="accordion-body">
+              
+              <table className='table table-hover tabla_basedatos'>
+                <thead className="table-green-titles">
+                  <tr>
+                    <th>Proyecto</th>
+                    <th>Creado por:</th>
+                    <th>Fecha</th>
+                    <th>Observ. 1</th>
+                    <th>Observ. 2</th>
+                    <th>Observ. 3</th>
+                    <th>Egreso</th>
+                    
+                    <th>Editar</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data &&
+                    data.Avances.map((u) => {
+                      return (
+                        <tr key={u._id}>
+                          <td>{u.proyecto.nombre}</td>
+                          <td>{u.creadoPor.nombre+ " "+ u.creadoPor.apellido}</td>
+                          <td>{u.fecha}</td>
+                          <td>{u.observaciones[0]}</td>
+                          <td>{u.observaciones[1]}</td>
+                          <td>{u.observaciones[2]}</td>
+                          <td>{u.fecha}</td>
+                          
+                          <td>
+                            <Link to={`/avances/editar/${u._id}`}>
+                              <i className='fas fa-pen text-yellow-600 hover:text-yellow-400 cursor-pointer' />
+                            </Link>
+                          </td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+              </table>
+
+            </div>
+          </div>
+        </div >
+      </div >
+
+      <div className="accordion" id="accordionExample">
+
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingTwo">
+            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree"
+              aria-expanded="false" aria-controls="collapseThree">
+              Mis Avances
+            </button>
+          </h2>
+          <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree"
             data-bs-parent="#accordionExample">
             <div className="accordion-body">
               
