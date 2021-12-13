ad110e11700ccbcd183b0d12a696bf73cb8e6ba4
josep-p <jose.pinzonp@gmail.com>
2021-12-13 03:09:30 -0500

    cambios a mutaaciones de editaar proyecto

--- src/graphql/proyecto/mutations.js
+++ src/graphql/proyecto/mutations.js
@@ -1,39 +1,18 @@
 import { gql } from '@apollo/client';
 
 const EDITAR_PROYECTO = gql`
-  mutation EditarProyecto(
-      $id: String!
-      $nombre: String
-      $presupuesto: Float
-      $fechaInicio: Date
-      $fechaFin: Date
-      $estado: Enum_EstadoProyecto
-      $fase: Enum_FaseProyecto
-      $lider: String
-  ){
-  editarProyecto(
-      _id: $id
-      nombre: $nombre
-      presupuesto: $presupuesto
-      fechaInicio: $fechaInicio
-      fechaFin: $fechaFin
-      estado: $estado
-      fase: $fase
-      lider: $lider
-      objetivos: $objetivos
-      ) {
+mutation EditarProyecto($_id: String!, $nombre: String, $presupuesto: Float, $fechaInicio: Date, $fechaFin: Date, $estado: Enum_EstadoProyecto, $fase: Enum_FaseProyecto, $lider: String, $objetivos: [crearObjetivo]) {
+  editarProyecto(_id: $_id, nombre: $nombre, presupuesto: $presupuesto, fechaInicio: $fechaInicio, fechaFin: $fechaFin, estado: $estado, fase: $fase, lider: $lider, objetivos: $objetivos) {
     _id
     nombre
     presupuesto
     fechaInicio
     fechaFin
-    estado
     fase
-    lider {
-      _id
-    }
+  estado
+   
     objetivos {
-      _id
+      
       descripcion
       tipo
     }
