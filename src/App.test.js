f77e4ef51613f6879c295eb16b4b20d153919f91
paulandreacl <paula150995@hotmail.com>
2021-12-17 11:35:13 -0500

    se edito app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Index from'./pages/home/Index'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Index />);
+  const linkElement = screen.getByText(/Bienvenido al Gestor de Proyectos/);
   expect(linkElement).toBeInTheDocument();
 });
