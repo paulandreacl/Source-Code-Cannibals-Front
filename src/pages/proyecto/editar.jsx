ea470f221571abeae4eb5147b226c82295fd5066
josep-p <90432076+josep-p@users.noreply.github.com>
2021-12-17 17:04:00 -0500

    Error para probar el workflow

--- src/pages/proyecto/editar.jsx
+++ src/pages/proyecto/editar.jsx
@@ -68,11 +68,7 @@ const EditarProyecto = () => {
   if (queryLoading) return <div>Loading...</div>;
 
   return (
-    <div className="flew flex-col w-full h-full items-center justify-center p-10">
-      <Link to="/proyectos">
-        <i className="fas fa-arrow-left text-gray-600 cursor-pointer font-bold text-xl hover:text-gray-900" />
-      </Link>
-      <h1 className="m-4 text-3xl text-gray-800 font-bold text-center">
+    ext-3xl text-gray-800 font-bold text-center">
         Editar Proyecto
       </h1>
       <form
