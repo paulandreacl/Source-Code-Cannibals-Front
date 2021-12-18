d1598f1ce6edf7cd2001e4a89d6f10afb28586ad
josep-p <90432076+josep-p@users.noreply.github.com>
2021-12-17 19:44:57 -0500

    Respaldo despliegue dic16 (#7)
    * Añadir columna para pasar el index
    
    * Td
    
    * td index
    
    * cambio
    
    * indice
    
    * ver boton
    
    * corregir errores
    
    * otra vez
    
    * tr
    
    * Display none
    
    * divs invisibles
    
    * index visible
    
    * Mas arriba
    
    * Añadir boton y formulario en fragmento
    
    * mapear el index con key =index
    
    * state
    
    * Sacar el queryData del tbody
    
    * tr con key
    
    * A ver con this.state
    
    * Dialogo flotante dentro del boton
    
    * Dialogo dentro de td
    
    * index en td
    
    * this.state
    
    * this.index
    
    * Intentar con key
    
    * const key
    
    * A ver si mapeando dos veces
    
    * Objetivo
    
    * Desde el componente objetivo
    
    * dentro de la tabla
    
    * refrescar pagina
    
    * Cambios al estilo del codigo
    
    * Cambios de estilo
    
    * Commit para probar el error de compilación y los flujos de CI
    
    * generar error
    
    * Probar errores
    
    * Desastre
    
    * Generar errores
    
    * Generacion de errores desde el flujo de trabajo
    
    * A ver si falla
    
    * Eliminar el contenido
    
    * Prueba de nuevo workflow
    
    * Prueba de cCI
    
    * Error para probar el workflow
    
    * Comentario agregado

--- src/pages/proyecto/editar.jsx
+++ src/pages/proyecto/editar.jsx
@@ -1,23 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import { GET_PROYECTO } from 'graphql/proyecto/queries';
-import { useParams, Link } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { useMutation } from '@apollo/client';
-import useFormData from 'hooks/useFormData';
-import Input from 'components/Input';
-import { EDITAR_PROYECTO, EDITAR_OBJETIVO, ELIMINAR_OBJETIVO } from 'graphql/proyecto/mutations';
-import { toast } from 'react-toastify';
-import ButtonLoading from 'components/ButtonLoading';
-import DropDown from 'components/DropDown'
-import { Enum_EstadoProyecto, Enum_FaseProyecto } from 'utils/enums';
-import PrivateComponent from 'components/PrivateComponent';
-import { Enum_TipoObjetivo } from 'utils/enums';
-import { nanoid } from 'nanoid';
-import { ObjContext, useObj } from 'context/objContext';
-import { Dialog } from '@material-ui/core';
-import ReactLoading from 'react-loading';
-
-
+import React, { useEffect, useState } from "react";
+import { GET_PROYECTO } from "graphql/proyecto/queries";
+import { useParams, Link } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { useMutation } from "@apollo/client";
+import useFormData from "hooks/useFormData";
+import Input from "components/Input";
+import {
+  EDITAR_PROYECTO,
+  EDITAR_OBJETIVO,
+  ELIMINAR_OBJETIVO,
+} from "graphql/proyecto/mutations";
+import { toast } from "react-toastify";
+import ButtonLoading from "components/ButtonLoading";
+import DropDown from "components/DropDown";
+import { Enum_EstadoProyecto, Enum_FaseProyecto } from "utils/enums";
+import PrivateComponent from "components/PrivateComponent";
+import { Enum_TipoObjetivo } from "utils/enums";
+import { nanoid } from "nanoid";
+import { ObjContext, useObj } from "context/objContext";
+import { Dialog } from "@material-ui/core";
+import ReactLoading from "react-loading";
 
 const EditarProyecto = () => {
   const [showDialog, setShowDialog] = useState(false);
@@ -29,58 +31,57 @@ const EditarProyecto = () => {
     error: queryError,
     data: queryData,
   } = useQuery(GET_PROYECTO, {
-    variables: { _id }
+    variables: { _id },
   });
 
-  const [editarProyecto, { data: mutationData, loading: mutationLoading, error: mutationError }] =
-    useMutation(EDITAR_PROYECTO);
-  console.log(mutationData)
+  const [
+    editarProyecto,
+    { data: mutationData, loading: mutationLoading, error: mutationError },
+  ] = useMutation(EDITAR_PROYECTO);
+  console.log(mutationData);
 
   const submitForm = (e) => {
     e.preventDefault();
     formData.presupuesto = parseFloat(formData.presupuesto);
     console.log(formData);
     editarProyecto({
-      variables: { _id, ...formData }
-    })
+      variables: { _id, ...formData },
+    });
   };
 
   useEffect(() => {
     if (mutationData) {
-      toast.success('Proyecto modificado correctamente');
+      toast.success("Proyecto modificado correctamente");
     }
   }, [mutationData]);
 
   useEffect(() => {
     if (mutationError) {
-      toast.error('Error modificando el proyecto');
+      toast.error("Error modificando el proyecto");
     }
 
     if (queryError) {
-      toast.error('Error consultando el proyecto');
+      toast.error("Error consultando el proyecto");
     }
   }, [queryError, mutationError]);
 
   if (queryLoading) return <div>Loading...</div>;
 
   return (
-    <div className='flew flex-col w-full h-full items-center justify-center p-10'>
-      <Link to='/proyectos'>
-        <i className='fas fa-arrow-left text-gray-600 cursor-pointer font-bold text-xl hover:text-gray-900' />
-      </Link>
-      <h1 className='m-4 text-3xl text-gray-800 font-bold text-center'>Editar Proyecto</h1>
+    ext-3xl text-gray-800 font-bold text-center">
+        Editar Proyecto
+      </h1>
       <form
         onSubmit={submitForm}
         onChange={updateFormData}
         ref={form}
-        className='row g-3 items-center justify-center'
+        className="row g-3 items-center justify-center"
       >
-
         <div className="col-md-3">
           <Input
-            label='Nombre del proyecto:'
-            type='text'
-            name='nombre'
+            label="Nombre del proyecto:"
+            type="text"
+            name="nombre"
             defaultValue={queryData.Proyecto.nombre}
             required={true}
           />
@@ -88,9 +89,9 @@ const EditarProyecto = () => {
 
         <div className="col-md-3">
           <Input
-            label='Presupuesto del proyecto:'
-            type='number'
-            name='presupuesto'
+            label="Presupuesto del proyecto:"
+            type="number"
+            name="presupuesto"
             defaultValue={queryData.Proyecto.presupuesto}
             required={true}
           />
@@ -98,112 +99,98 @@ const EditarProyecto = () => {
         <div className="col-md-3">
           <Input
             readOnly
-            label='Fecha Inicio:'
-            type='Date'
-            name='fechaInicio'
-            defaultValue={!queryData.Proyecto.fechaInicio ? '' : queryData.Proyecto.fechaInicio.slice(0, -14)}
+            label="Fecha Inicio:"
+            type="Date"
+            name="fechaInicio"
+            defaultValue={
+              !queryData.Proyecto.fechaInicio
+                ? ""
+                : queryData.Proyecto.fechaInicio.slice(0, -14)
+            }
             required={true}
           />
         </div>
         <div className="col-md-3">
           <Input
             readOnly
-            label='Fecha Fin:'
-            type='Date'
-            name='fechaFin'
-            defaultValue={!queryData.Proyecto.fechaFin ? '' : queryData.Proyecto.fechaFin.slice(0, -14)}
+            label="Fecha Fin:"
+            type="Date"
+            name="fechaFin"
+            defaultValue={
+              !queryData.Proyecto.fechaFin
+                ? ""
+                : queryData.Proyecto.fechaFin.slice(0, -14)
+            }
             required={true}
           />
         </div>
 
-        <PrivateComponent roleList='ADMINISTRADOR'>
+        <PrivateComponent roleList="ADMINISTRADOR">
           <div className="col-md-3">
             <DropDown
-              label='Estado del proyecto:'
-              name='estado'
+              label="Estado del proyecto:"
+              name="estado"
               defaultValue={queryData.Proyecto.estado}
               required={true}
               options={Enum_EstadoProyecto}
-
             />
           </div>
           <div className="col-md-3">
-
             <DropDown
-              label='Fase del proyecto:'
-              name='fase'
+              label="Fase del proyecto:"
+              name="fase"
               defaultValue={queryData.Proyecto.fase}
               required={true}
               options={Enum_FaseProyecto}
-
             />
           </div>
         </PrivateComponent>
 
-
         <div>
-          <h2 className='m-4 text-xl text-gray-800 font-bold text-center'>Objetivos</h2>
-          <table className='table table-hover tabla_basedatos'>
+        <div>
+          <h2 className="m-4 text-xl text-gray-800 font-bold text-center">
+            Objetivos
+          </h2>
+          <table className="table table-hover tabla_basedatos">
             <th>Descripción</th>
             <th>Tipo</th>
             <th>Editar</th>
-            {queryData.Proyecto.objetivos.map((objetivo, index) => (
-              <tbody>
-                <td>{objetivo.descripcion}</td>
-                <td>{objetivo.tipo}</td>
-                <td>{
-                  <button type='button' onClick={() => setShowEditDialog(true)}>
-                    <i className='fas fa-pen mx-2 text-yellow-500 hover:text-yellow-200 cursor-pointer' />
-                  </button>
-                }</td>
-                <div>
+            <tbody>
+              {queryData.Proyecto.objetivos.map(
+                      
 
-                  <Dialog open={showEditDialog} onClose={() => setShowEditDialog(false)}>
-                    <EditarObjetivo
-                      descripcion={objetivo.descripcion}
-                      tipo={objetivo.tipo}
-                      idProyecto={_id}
-                      index={index}
-                      setShowEditDialog={setShowEditDialog}
-
-                    />
-                  </Dialog>
-                </div>
-
-              </tbody>
-            ))
-            }
-          </table>
-        </div>
-
-
-        <ButtonLoading className="btn-primary"
+        <ButtonLoading
+          className="btn-primary"
+espaldoDespliegueDic16
           disabled={Object.keys(formData).length === 0}
           loading={mutationLoading}
-          text='Confirmar'
+          text="Confirmar"
         />
-
       </form>
       {/* {queryData.Proyecto.objetivos.map((objetivo) =>(<Objetivo
            descripcion={objetivo.descripcion}
            tipo={objetivo.tipo}                          
            idProyecto={_id}
            setShowEditDialog={setShowEditDialog}
-           />))} */}
+           />))} 
+           
+      comentrio pra hcer pull request despues*/}
 
-
+      
     </div>
   );
 };
 
-// desde acá esta añadido 
-
+// desde acá esta añadido
 
 const FormEditProyecto = ({ _id }) => {
   const { form, formData, updateFormData } = useFormData();
 
   // falta capturar error de la mutacion
   // falta toast de success
+
+          disabled=
+pruebasCI
   const [editarProyecto, { loading }] = useMutation(EDITAR_PROYECTO);
 
   const submitForm = (e) => {
@@ -211,70 +198,12 @@ const FormEditProyecto = ({ _id }) => {
     editarProyecto({
       variables: {
         _id,
-        campos: formData,
-      },
-    });
-  };
-
   return (
-    <div className='p-4'>
-      <h1 className='font-bold'>Modificar Estado del Proyecto</h1>
-      <form
-        ref={form}
-        onChange={updateFormData}
-        onSubmit={submitForm}
-        className='flex flex-col items-center'
-      >
-        <DropDown
-          label='Estado del Proyecto'
-          name='estado'
-          options={Enum_EstadoProyecto}
-        />
-        <ButtonLoading disabled={false} loading={loading} text='Confirmar' />
-      </form>
-    </div>
-  );
-};
-
-const Objetivo = ({ index, _id, idProyecto, tipo, descripcion }) => {
-  const [showEditDialog, setShowEditDialog] = useState(false);
-  const [
-    eliminarObjetivo,
-    { data: dataMutationEliminar, loading: eliminarLoading },
-  ] = useMutation(ELIMINAR_OBJETIVO, {
-    refetchQueries: [{ query: GET_PROYECTO }],
-  });
-
-  useEffect(() => {
-    if (dataMutationEliminar) {
-      toast.success('objetivo eliminado satisfactoriamente');
-    }
-  }, [dataMutationEliminar]);
-
-  const ejecutarEliminacion = () => {
-    eliminarObjetivo({ variables: { idProyecto, idObjetivo: _id } });
-  };
-
-  if (eliminarLoading)
-    return (
-      <ReactLoading
-        data-testid='loading-in-button'
-        type='spin'
-        height={100}
-        width={100}
-      />
-    );
-  return (
-    <div className='mx-5 my-4 bg-gray-50 p-8 rounded-lg flex flex-col items-center justify-center shadow-xl'>
-      <div className='text-lg font-bold'>{tipo}</div>
-      <div>{descripcion}</div>
-      <PrivateComponent roleList={['ADMINISTRADOR', 'LIDER']}>
-        <div className='flex my-2'>
-          <button type='button' onClick={() => setShowEditDialog(true)}>
-            <i className='fas fa-pen mx-2 text-yellow-500 hover:text-yellow-200 cursor-pointer' />
-          </button>
-          <button type='button' onClick={ejecutarEliminacion}>
-            <i className='fas fa-trash mx-2 text-red-500 hover:text-red-200 cursor-pointer' />
+    <>
+      <div>
+        <div className="flex my-2">
+          <button type="button" onClick={() => setShowEditDialog(true)}>
+            <i className="fas fa-pen mx-2 text-yellow-500 hover:text-yellow-200 cursor-pointer" />
           </button>
         </div>
         <Dialog open={showEditDialog} onClose={() => setShowEditDialog(false)}>
@@ -286,11 +215,10 @@ const Objetivo = ({ index, _id, idProyecto, tipo, descripcion }) => {
             setShowEditDialog={setShowEditDialog}
           />
         </Dialog>
-      </PrivateComponent>
-    </div>
+      </div>
+    </>
   );
 };
-
 
 const EditarObjetivo = ({
   descripcion,
@@ -301,7 +229,6 @@ const EditarObjetivo = ({
 }) => {
   const { form, formData, updateFormData } = useFormData();
 
-
   const [editarObjetivo, { data: dataMutation, loading }] = useMutation(
     EDITAR_OBJETIVO,
     {
@@ -311,42 +238,33 @@ const EditarObjetivo = ({
 
   useEffect(() => {
     if (dataMutation) {
-      toast.success('Objetivo editado con exito');
+      toast.success("Objetivo editado con exito");
       setShowEditDialog(false);
     }
-  }, [dataMutation, setShowEditDialog]);
-
-  const submitForm = (e) => {
-    e.preventDefault();
-    editarObjetivo({
-      variables: {
-        idProyecto,
-        indexObjetivo: index,
-        campos: formData,
       },
     }).catch((error) => {
-      toast.error('Error editando el objetivo', error);
+      toast.error("Error editando el objetivo", error);
     });
   };
   return (
-    <div className='p-4'>
-      <h1 className='text-2xl font-bold text-gray-900'>Editar Objetivo</h1>
+    <div className="p-4">
+      <h1 className="text-2xl font-bold text-gray-900">Editar Objetivo</h1>
       <form ref={form} onChange={updateFormData} onSubmit={submitForm}>
         <DropDown
-          label='Tipo de Objetivo'
-          name='tipo'
+          label="Tipo de Objetivo"
+          name="tipo"
           required
           options={Enum_TipoObjetivo}
           defaultValue={tipo}
         />
         <Input
-          label='Descripcion del objetivo'
-          name='descripcion'
+          label="Descripcion del objetivo"
+          name="descripcion"
           required
           defaultValue={descripcion}
         />
         <ButtonLoading
-          text='Confirmar'
+          text="Confirmar"
           disabled={Object.keys(formData).length === 0}
           loading={loading}
         />
